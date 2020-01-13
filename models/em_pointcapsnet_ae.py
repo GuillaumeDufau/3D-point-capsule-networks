54ac2653f1282df3b997ac252a83fc7328fb984b
murrman95 <christopher.murray10@gmail.com>
2020-01-13 09:41:40 +0100

    It runs now :^)

--- models/em_pointcapsnet_ae.py
+++ models/em_pointcapsnet_ae.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep  6 17:57:49 2018
-
+ 
 @author: zhao
 """
-
+ 
 from __future__ import print_function
 import os
 import torch
@@ -17,7 +17,7 @@ import torch.nn.functional as F
 from collections import OrderedDict
 import sys
 import math
-
+ 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #sys.path.append(os.path.join(BASE_DIR, 'nndistance'))
 #from modules.nnd import NNDModule
@@ -25,8 +25,8 @@ import torch_nndistance as NND
 #distChamfer = NNDModule()
 distChamfer = NND.nnd
 USE_CUDA = True
-
-
+ 
+ 
 class ConvLayer(nn.Module):
     def __init__(self):
         super(ConvLayer, self).__init__()
@@ -34,13 +34,13 @@ class ConvLayer(nn.Module):
         self.conv2 = torch.nn.Conv1d(64, 128, 1)
         self.bn1 = nn.BatchNorm1d(64)
         self.bn2 = nn.BatchNorm1d(128)
-
+ 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         return x
-
-
+ 
+ 
 class PrimaryPointCapsLayer(nn.Module):
     def __init__(self, prim_vec_size=8, num_points=2048):
         super(PrimaryPointCapsLayer, self).__init__()
@@ -51,18 +51,17 @@ class PrimaryPointCapsLayer(nn.Module):
                 ('mp1', torch.nn.MaxPool1d(num_points)),
             ]))
             for _ in range(prim_vec_size + 1)])
-
+ 
     def forward(self, x):
         u = [capsule(x) for capsule in self.capsules]
         u = torch.stack(u, dim=2)
-        print(u.shape)
         return u.view(u.shape[0],1,1,u.shape[1]*(4*4+1))
-
+ 
 class ConvCaps(nn.Module):
     r"""Create a convolutional capsule layer
     that transfer capsule layer L to capsule layer L+1
     by EM routing.
-
+ 
     Args:
         B: input number of types of capsules
         C: output number on types of capsules
@@ -72,14 +71,14 @@ class ConvCaps(nn.Module):
         iters: number of EM iterations
         coor_add: use scaled coordinate addition or not
         w_shared: share transformation matrix across w*h.
-
+ 
     Shape:
         input:  (*, h,  w, B*(P*P+1))
         output: (*, h', w', C*(P*P+1))
         h', w' is computed the same way as convolution layer
         parameter size is: K*K*B*C*P*P + B*P*P
     """
-    def __init__(self, B=1024, C=64, K=1, P=4, stride=1, iters=3,
+    def __init__(self, B=1024, C=32, K=1, P=4, stride=1, iters=3,
                  coor_add=False, w_shared=False):
         super(ConvCaps, self).__init__()
         # TODO: lambda scheduler
@@ -110,14 +109,14 @@ class ConvCaps(nn.Module):
         # op
         self.sigmoid = nn.Sigmoid()
         self.softmax = nn.Softmax(dim=2)
-
+ 
     def m_step(self, a_in, r, v, eps, b, B, C, psize):
         """
             \mu^h_j = \dfrac{\sum_i r_{ij} V^h_{ij}}{\sum_i r_{ij}}
             (\sigma^h_j)^2 = \dfrac{\sum_i r_{ij} (V^h_{ij} - mu^h_j)^2}{\sum_i r_{ij}}
             cost_h = (\beta_u + log \sigma^h_j) * \sum_i r_{ij}
             a_j = logistic(\lambda * (\beta_a - \sum_h cost_h))
-
+ 
             Input:
                 a_in:      (b, C, 1)
                 r:         (b, B, C, 1)
@@ -135,26 +134,26 @@ class ConvCaps(nn.Module):
         r_sum = r.sum(dim=1, keepdim=True)
         coeff = r / (r_sum + eps)
         coeff = coeff.view(b, B, C, 1)
-
+ 
         mu = torch.sum(coeff * v, dim=1, keepdim=True)
         sigma_sq = torch.sum(coeff * (v - mu)**2, dim=1, keepdim=True) + eps
-
+ 
         r_sum = r_sum.view(b, C, 1)
         sigma_sq = sigma_sq.view(b, C, psize)
         cost_h = (self.beta_u.view(C, 1) + torch.log(sigma_sq.sqrt())) * r_sum
-
+ 
         a_out = self.sigmoid(self._lambda*(self.beta_a - cost_h.sum(dim=2)))
         sigma_sq = sigma_sq.view(b, 1, C, psize)
-
+ 
         return a_out, mu, sigma_sq
-
+ 
     def e_step(self, mu, sigma_sq, a_out, v, eps, b, C):
         """
             ln_p_j = sum_h \dfrac{(\V^h_{ij} - \mu^h_j)^2}{2 \sigma^h_j}
                     - sum_h ln(\sigma^h_j) - 0.5*\sum_h ln(2*\pi)
             r = softmax(ln(a_j*p_j))
               = softmax(ln(a_j) + ln(p_j))
-
+ 
             Input:
                 mu:        (b, 1, C, P*P)
                 sigma:     (b, 1, C, P*P)
@@ -169,11 +168,11 @@ class ConvCaps(nn.Module):
         ln_p_j_h = -1. * (v - mu)**2 / (2 * sigma_sq) \
                     - torch.log(sigma_sq.sqrt()) \
                     - 0.5*self.ln_2pi
-
+ 
         ln_ap = ln_p_j_h.sum(dim=3) + torch.log(a_out.view(b, 1, C))
         r = self.softmax(ln_ap)
         return r
-
+ 
     def caps_em_routing(self, v, a_in, C, eps):
         """
             Input:
@@ -182,7 +181,7 @@ class ConvCaps(nn.Module):
             Output:
                 mu:        (b, 1, C, P*P)
                 a_out:     (b, C, 1)
-
+ 
             Note that some dimensions are merged
             for computation convenient, that is
             `b == batch_size*oh*ow`,
@@ -192,15 +191,15 @@ class ConvCaps(nn.Module):
         b, B, c, psize = v.shape
         assert c == C
         assert (b, B, 1) == a_in.shape
-
+ 
         r = torch.cuda.FloatTensor(b, B, C).fill_(1./C)
         for iter_ in range(self.iters):
             a_out, mu, sigma_sq = self.m_step(a_in, r, v, eps, b, B, C, psize)
             if iter_ < self.iters - 1:
                 r = self.e_step(mu, sigma_sq, a_out, v, eps, b, C)
-
+ 
         return mu, a_out
-
+ 
     def add_pathes(self, x, B, K, psize, stride):
         """
             Shape:
@@ -208,9 +207,7 @@ class ConvCaps(nn.Module):
                 Output:    (b, H', W', K, K, B*(P*P+1))
         """
         b, h, w, c = x.shape
-        print(self.B)
-        print(psize)
-        print(B*(psize+1))
+ 
         assert h == w
         assert c == B*(psize+1)
         oh = ow = int(((h - K )/stride)+ 1) # moein - changed from: oh = ow = int((h - K + 1) / stride)
@@ -221,7 +218,7 @@ class ConvCaps(nn.Module):
         x = x[:, :, :, idxs, :]
         x = x.permute(0, 1, 3, 2, 4, 5).contiguous()
         return x, oh, ow
-
+ 
     def transform_view(self, x, w, C, P, w_shared=False):
         """
             For conv_caps:
@@ -233,18 +230,18 @@ class ConvCaps(nn.Module):
         """
         b, B, psize = x.shape
         assert psize == P*P
-
+ 
         x = x.view(b, B, 1, P, P)
         if w_shared:
             hw = int(B / w.size(1))
             w = w.repeat(1, hw, 1, 1, 1)
-
+ 
         w = w.repeat(b, 1, 1, 1, 1)
         x = x.repeat(1, 1, C, 1, 1)
         v = torch.matmul(x, w)
         v = v.view(b, B, C, P*P)
         return v
-
+ 
     def add_coord(self, v, b, h, w, B, C, psize):
         """
             Shape:
@@ -261,22 +258,21 @@ class ConvCaps(nn.Module):
         v = v + coor_h + coor_w
         v = v.view(b, h*w*B, C, psize)
         return v
-
+ 
     def forward(self, x):
-        print(x.shape)
-        print(self.B)
+ 
         b, h, w, c = x.shape
         if not self.w_shared:
             # add patches
             x, oh, ow = self.add_pathes(x, 1024, 1, 16, self.stride)
-
+ 
             # transform view
             p_in = x[:, :, :, :, :, :self.B*self.psize].contiguous()
             a_in = x[:, :, :, :, :, self.B*self.psize:].contiguous()
             p_in = p_in.view(b*oh*ow, self.K*self.K*self.B, self.psize)
             a_in = a_in.view(b*oh*ow, self.K*self.K*self.B, 1)
             v = self.transform_view(p_in, self.weights, self.C, self.P)
-
+ 
             # em_routing
             p_out, a_out = self.caps_em_routing(v, a_in, self.C, self.eps)
             p_out = p_out.view(b, oh, ow, self.C*self.psize)
@@ -290,20 +286,20 @@ class ConvCaps(nn.Module):
             p_in = p_in.view(b, h*w*self.B, self.psize)
             a_in = x[:, :, :, self.B*self.psize:].contiguous()
             a_in = a_in.view(b, h*w*self.B, 1)
-
+ 
             # transform view
             v = self.transform_view(p_in, self.weights, self.C, self.P, self.w_shared)
-
+ 
             # coor_add
             if self.coor_add:
                 v = self.add_coord(v, b, h, w, self.B, self.C, self.psize)
-
+ 
             # em_routing
             _, out = self.caps_em_routing(v, a_in, self.C, self.eps)
-
+ 
         return out
-
-
+ 
+ 
 # Old
 '''
 class LatentCapsLayer(nn.Module):
@@ -313,7 +309,7 @@ class LatentCapsLayer(nn.Module):
         self.prim_caps_size = prim_caps_size
         self.latent_caps_size = latent_caps_size
         self.W = nn.Parameter(0.01*torch.randn(latent_caps_size, prim_caps_size, latent_vec_size, prim_vec_size))
-
+ 
     def forward(self, x):
         u_hat = torch.squeeze(torch.matmul(self.W, x[:, None, :, :, None]), dim=-1)
         u_hat_detached = u_hat.detach()
@@ -327,14 +323,14 @@ class LatentCapsLayer(nn.Module):
                 v_j = self.squash(torch.sum(c_ij[:, :, :, None] * u_hat_detached, dim=-2, keepdim=True))
                 b_ij = b_ij + torch.sum(v_j * u_hat_detached, dim=-1)
         return v_j.squeeze(-2)
-
+ 
     def squash(self, input_tensor):
         squared_norm = (input_tensor ** 2).sum(-1, keepdim=True)
         output_tensor = squared_norm * input_tensor / \
             ((1. + squared_norm) * torch.sqrt(squared_norm))
         return output_tensor
 '''
-
+ 
 class PointGenCon(nn.Module):
     def __init__(self, bottleneck_size=2500):
         self.bottleneck_size = bottleneck_size
@@ -347,33 +343,36 @@ class PointGenCon(nn.Module):
         self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size)
         self.bn2 = torch.nn.BatchNorm1d(int(self.bottleneck_size/2))
         self.bn3 = torch.nn.BatchNorm1d(int(self.bottleneck_size/4))
-
+ 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         x = self.th(self.conv4(x))
         return x
-
+ 
 class CapsDecoder(nn.Module):
     def __init__(self, latent_caps_size, latent_vec_size, num_points):
         super(CapsDecoder, self).__init__()
         self.latent_caps_size = latent_caps_size
-        self.bottleneck_size=latent_vec_size
+        self.bottleneck_size=32
         self.num_points = num_points
         self.nb_primitives=int(num_points/latent_caps_size)
         self.decoder = nn.ModuleList(
             [PointGenCon(bottleneck_size=self.bottleneck_size+2) for i in range(0, self.nb_primitives)])
     def forward(self, x):
+        x = x[:,:,:,:-self.latent_caps_size]
+ 
         outs = []
         for i in range(0, self.nb_primitives):
-            rand_grid = Variable(torch.cuda.FloatTensor(x.size(0), 2, self.latent_caps_size))
+            rand_grid = Variable(torch.cuda.FloatTensor(x.size(0), 2, 32))
             rand_grid.data.uniform_(0, 1)
+            x = x.view(x.shape[0],32,32)
             y = torch.cat((rand_grid, x.transpose(2, 1)), 1).contiguous()
             outs.append(self.decoder[i](y))
         return torch.cat(outs, 2).contiguous()
-
-
+ 
+ 
 class PointCapsNet(nn.Module):
     def __init__(self, prim_caps_size, prim_vec_size, latent_caps_size, latent_vec_size, num_points):
         super(PointCapsNet, self).__init__()
@@ -381,24 +380,24 @@ class PointCapsNet(nn.Module):
         self.primary_point_caps_layer = PrimaryPointCapsLayer(prim_vec_size, num_points)
         self.latent_caps_layer = ConvCaps(B=prim_caps_size,C=latent_caps_size,P=4,stride=1,K=1)
         self.caps_decoder = CapsDecoder(latent_caps_size,latent_vec_size, num_points)
-
+ 
     def forward(self, data):
         x1 = self.conv_layer(data)
         x2 = self.primary_point_caps_layer(x1)
         latent_capsules = self.latent_caps_layer(x2)
         reconstructions = self.caps_decoder(latent_capsules)
         return latent_capsules, reconstructions
-
+ 
     def loss(self, data, reconstructions):
          return self.reconstruction_loss(data, reconstructions)
-
+ 
     def reconstruction_loss(self, data, reconstructions):
         data_ = data.transpose(2, 1).contiguous()
         reconstructions_ = reconstructions.transpose(2, 1).contiguous()
         dist1, dist2 = distChamfer(data_, reconstructions_)
         loss = (torch.mean(dist1)) + (torch.mean(dist2))
         return loss
-
+ 
 # This is a single network which can decode the point cloud from pre-saved latent capsules
 class PointCapsNetDecoder(nn.Module):
     def __init__(self, prim_caps_size, prim_vec_size, digit_caps_size, digit_vec_size, num_points):
@@ -407,33 +406,36 @@ class PointCapsNetDecoder(nn.Module):
     def forward(self, latent_capsules):
         reconstructions = self.caps_decoder(latent_capsules)
         return  reconstructions
-
+ 
 if __name__ == '__main__':
     USE_CUDA = True
     batch_size=8
-
+ 
     prim_caps_size=1024
     prim_vec_size=16
-
-    latent_caps_size=64
+ 
+    latent_caps_size=32
     latent_vec_size=16
-
+ 
     num_points=2048
-
+ 
     point_caps_ae = PointCapsNet(prim_caps_size,prim_vec_size,latent_caps_size,latent_vec_size,num_points)
     point_caps_ae=torch.nn.DataParallel(point_caps_ae).cuda()
-
+ 
     rand_data=torch.rand(batch_size,num_points, 3)
     rand_data = Variable(rand_data)
     rand_data = rand_data.transpose(2, 1)
     rand_data=rand_data.cuda()
-
+ 
     codewords,reconstruction=point_caps_ae(rand_data)
-
+ 
     rand_data_ = rand_data.transpose(2, 1).contiguous()
     reconstruction_ = reconstruction.transpose(2, 1).contiguous()
-
+ 
     dist1, dist2 = distChamfer(rand_data_, reconstruction_)
     loss = (torch.mean(dist1)) + (torch.mean(dist2))
     print(loss.item())
+ 
+ 
+
 
